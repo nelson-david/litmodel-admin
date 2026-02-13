b10c52f7fd1d7d296edad7007459a7e63f27345c
nelson-david <larva.code15@gmail.com>
2026-02-13 22:07:48 +0100

    Modified config

--- src/config.ts
+++ src/config.ts
@@ -2,35 +2,35 @@ import { toast } from "react-toastify";
 
 export const ADMIN_TOKEN = "thisistheadmintoken";
 // export const API_URL = "http://localhost:6500/api/v1";
-export const API_URL = "https://litmodel-api.vercel.app/api/v1";
+export const API_URL = "https://litmodel-server.onrender.com/api/v1";
 
 export const successToast = (success: string) =>
-    toast.success(`${success}`, {
-        position: "top-right",
-        theme: "dark",
-        autoClose: 2500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-    });
+	toast.success(`${success}`, {
+		position: "top-right",
+		theme: "dark",
+		autoClose: 2500,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+	});
 export const errorToast = (error: string) =>
-    toast.error(`${error}`, {
-        position: "top-right",
-        theme: "dark",
-        autoClose: 2500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-    });
+	toast.error(`${error}`, {
+		position: "top-right",
+		theme: "dark",
+		autoClose: 2500,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+	});
 export const warningToast = (warning: string) =>
-    toast.warning(`${warning}`, {
-        position: "top-right",
-        theme: "dark",
-        autoClose: 2500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-    });
+	toast.warning(`${warning}`, {
+		position: "top-right",
+		theme: "dark",
+		autoClose: 2500,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+	});
